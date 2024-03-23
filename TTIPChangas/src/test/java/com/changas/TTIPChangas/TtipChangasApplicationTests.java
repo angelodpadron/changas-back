09fe701ea76da6be770cc35ccb53a3feacd2e49b
Leandroa77 <leandroarroyo77@gmail.com>
2024-03-23 20:57:40 -0300

    spring and gradle files

---
+++ TTIPChangas/src/test/java/com/changas/TTIPChangas/TtipChangasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changas.TTIPChangas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtipChangasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
