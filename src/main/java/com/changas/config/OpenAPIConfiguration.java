b83133ef8e29da48c0b1eb43becdce62c81f9a4d
Angelo Padron <padron891@gmail.com>
2024-03-27 13:23:35 -0300

    Agregar dependencias y configuración:
    - Jacoco
    - Lombok
    - SpringDoc

---
+++ src/main/java/com/changas/config/OpenAPIConfiguration.java
@@ -0,0 +1,35 @@
+package com.changas.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+    @Value("${app.base-url}")
+    private String serverUrl;
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl(this.serverUrl);
+
+        Contact contact = new Contact();
+        contact.setName("Changas Support Team");
+        contact.setEmail("support@changas.com");
+
+        Info info = new Info()
+                .title("Changas API")
+                .version("0.1")
+                .description("This API exposes the main endpoints of Changas")
+                .contact(contact);
+
+        return new OpenAPI().info(info).servers(List.of(server));
+    }
+}
