1171923ebe60bab71c8ab6e23f8cda241c38d95c
Angelo Padron <padron891@gmail.com>
2024-04-30 16:23:13 -0300

    Arreglo menor en changa repository

--- src/main/java/com/changas/repository/ChangaRepository.java
+++ src/main/java/com/changas/repository/ChangaRepository.java
@@ -1,6 +1,7 @@
 package com.changas.repository;
 
 import com.changas.model.Changa;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,9 @@ import java.util.Set;
 
 @Repository
 public interface ChangaRepository extends CrudRepository<Changa, Long> {
+    @Query("SELECT c FROM Changa c JOIN c.topics t WHERE LOWER(t) IN :topics")
     Set<Changa> findChangasByTopics(@Param("topics") Set<String> topics);
     Set<Changa> findByTitleContainingIgnoreCase(String title);
+    @Query("SELECT c FROM Changa c JOIN c.topics t WHERE LOWER(c.title) LIKE LOWER(:title) AND LOWER(t) IN :topics")
     Set<Changa> findByTitleAndTopics(@Param("title") String title, @Param("topics") Set<String> topics);
 }
