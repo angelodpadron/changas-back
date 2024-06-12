1e4789a14d81b7439cbc0977872a91dad636985b
Angelo Padron <padron891@gmail.com>
2024-06-12 17:30:32 -0300

    Fix tests

--- src/test/java/com/changas/service/ChangaServiceTest.java
+++ src/test/java/com/changas/service/ChangaServiceTest.java
@@ -1,10 +1,12 @@
 package com.changas.service;
 
+import com.changas.dto.area.ServiceAreaRequest;
 import com.changas.dto.changa.ChangaOverviewDTO;
 import com.changas.dto.changa.CreateChangaRequest;
 import com.changas.exceptions.customer.CustomerNotAuthenticatedException;
 import com.changas.model.Changa;
 import com.changas.model.Customer;
+import com.changas.model.ServiceArea;
 import com.changas.repository.ChangaRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -49,8 +51,14 @@ class ChangaServiceTest {
         Changa changa1 = mock(Changa.class);
         Changa changa2 = mock(Changa.class);
 
+        ServiceArea serviceArea = mock(ServiceArea.class);
+
+
         when(changa1.getProvider()).thenReturn(testCustomer);
         when(changa2.getProvider()).thenReturn(testCustomer);
+        when(changa1.getServiceArea()).thenReturn(serviceArea);
+        when(changa2.getServiceArea()).thenReturn(serviceArea);
+
         when(changaRepository.findChangasByTopics(any(), any(Integer.class))).thenReturn(new HashSet<>(List.of(changa1, changa2)));
 
         Set<ChangaOverviewDTO> filtered = changaService.findChangasByTopic(new HashSet<>(List.of("Plumbing", "Wielding")));
@@ -63,6 +71,7 @@ class ChangaServiceTest {
     @DisplayName("A created changa gets saved into the customer's posts collection")
     void savingCreatedChangaOnCustomerTest() throws Exception {
         CreateChangaRequest createChangaRequest = createChangaRequest();
+
         when(authService.getCustomerAuthenticated()).thenReturn(testCustomer);
 
         changaService.createChanga(createChangaRequest);
@@ -97,7 +106,8 @@ class ChangaServiceTest {
     }
 
     private CreateChangaRequest createChangaRequest() {
-        return new CreateChangaRequest("Changa Title", "Description", "https://image.org/image.jpg", new HashSet<>(), null);
+        ServiceAreaRequest serviceAreaRequest = new ServiceAreaRequest("Address", new double[]{-58.2912458, -34.7955703});
+        return new CreateChangaRequest("Changa Title", "Description", "https://image.org/image.jpg", new HashSet<>(), serviceAreaRequest);
     }
 
 }
