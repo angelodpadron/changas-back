ed675a07c865ba91e5592343bcaf8ab998e8eab4
Angelo Padron <padron891@gmail.com>
2024-06-13 12:04:07 -0300

    Agregar tests para area de servicio

---
+++ src/test/java/com/changas/model/ServiceAreaTest.java
@@ -0,0 +1,35 @@
+package com.changas.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Point;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ServiceAreaTest {
+
+    @DisplayName("A service area can generate a point by providing coordinates")
+    @Test
+    void serviceAreaCanGeneratePointFromCoordinatesTest() {
+        double x = -58.2912458;
+        double y = -34.7955703;
+        double[] coordinates = new double[]{x, y};
+        Point point = ServiceArea.createPoint(coordinates);
+        assertEquals(x, point.getX());
+        assertEquals(y, point.getY());
+    }
+
+    @DisplayName("Can generate a service area by providing a name and coordinates")
+    @Test
+    void generateServiceAreaTest() {
+        double x = -58.2912458;
+        double y = -34.7955703;
+        double[] coordinates = new double[]{x, y};
+        ServiceArea generated = ServiceArea.fromData("Address", coordinates);
+
+        assertEquals("Address", generated.getName());
+        assertEquals(x, generated.getLocation().getX());
+        assertEquals(y, generated.getLocation().getY());
+    }
+
+}
